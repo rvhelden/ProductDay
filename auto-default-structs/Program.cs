2febf32f4783c7e0480b78e6d7a024c32153b4c1
Ronald van Helden <ronald.van.helden@wortell.nl>
2023-01-16 10:50:19 +0100

    AutoDefault

--- auto-default-structs/Program.cs
+++ auto-default-structs/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿struct Sample
+{
+    public int Number1;
+    public int Number2;
+
+    public Sample()
+    {
+        Number1 = 0;
+        Number2 = 0;
+    }
+
+    public Sample(int number1, int number2)
+    {
+        Number1 = number1;
+        Number2 = number2;
+    }
+}

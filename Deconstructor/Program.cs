1aafdbc85bfd1f5e3e81dd07085f0833f57a0e65
Ronald van Helden <ronald.van.helden@wortell.nl>
2023-01-18 08:54:22 +0100

    Revert "Removed deconstruct"
    This reverts commit ddf67b269eb904f642eeec28a7e69015693f194b.

--- Deconstructor/Program.cs
+++ Deconstructor/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿var point = new Point(1, 2);
+
+var (x, y) = point;
+
+
+
+public readonly struct Point
+{
+    public readonly int X;
+    public readonly int Y;
+
+    public Point(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public void Deconstruct(out int x, out int y)
+    {
+        x = this.X;
+        y = this.Y;
+    }
+}
+
+
+
+
+
+public static class Extensions
+{
+    /*public static void Deconstruct(this Point point, out int x, out int y)
+    {
+        x = point.X;
+        y = point.Y;
+    }*/
+}

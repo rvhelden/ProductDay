19c0315d88021f6aee8464fcd1910a3293bc9049
Ronald van Helden <ronald.van.helden@wortell.nl>
2023-01-18 15:16:14 +0100

    Revert "Removed required members"
    This reverts commit b4de5a6f6a52e92c2470932b6f6e9fded80ce827.

--- required-members/Program.cs
+++ required-members/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿var entity = new Entity
+{
+    Name = "Car"
+};
+
+Console.WriteLine(entity.Name);
+
+public class Entity
+{
+    //public required string Name { get; init; }
+    public string Name { get; init; }
+}

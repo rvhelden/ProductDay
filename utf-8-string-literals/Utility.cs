e9b64b8baf76245c6fa5c32d71a0e164afd55936
Ronald van Helden <ronald.van.helden@wortell.nl>
2023-01-18 15:14:47 +0100

    Revert "Removed string literals"
    This reverts commit f069814db153d0e1b5c78bb7ab744a95825b3db6.

--- utf-8-string-literals/Utility.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace utf_8_string_literals;
-
-public class Utility
-{
-    public static void Print(ReadOnlySpan<byte> text)
-    {
-        Console.WriteLine(string.Join(" ", text.ToArray().Select(x => $"0x{x}")));
-    }
-}

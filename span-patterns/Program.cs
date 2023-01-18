2310f941c1aa8bd937647466833633803b955f26
Ronald van Helden <ronald.van.helden@wortell.nl>
2023-01-18 15:15:43 +0100

    Revert "Removed span patterns"
    This reverts commit f68f783c1c06ab4062553c2433e1a5d47c4fa697.

--- span-patterns/Program.cs
+++ span-patterns/Program.cs
@@ -1,1 +1,15 @@
 ﻿
+if (Method("hoi"))
+{
+    Console.WriteLine("It is hoi");
+}
+
+if (Method("cloud"))
+{
+    Console.WriteLine("It is cloud");
+}
+
+bool Method(ReadOnlySpan<char> text)
+{
+    return text is "hoi";
+}

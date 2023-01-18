f069814db153d0e1b5c78bb7ab744a95825b3db6
Ronald van Helden <ronald.van.helden@wortell.nl>
2023-01-18 08:49:44 +0100

    Removed string literals

--- utf-8-string-literals/Program.cs
+++ utf-8-string-literals/Program.cs
@@ -1,8 +1,1 @@
 ﻿
-using System.Text;
-
-ReadOnlySpan<byte> utf8 = "123"u8;
-ReadOnlySpan<byte> utf16 = Encoding.Unicode.GetBytes("123");
-
-Console.WriteLine(string.Join(" ", utf8.ToArray().Select(x => $"0x{x}")));
-Console.WriteLine(string.Join(" ", utf16.ToArray().Select(x => $"0x{x}")));
---
+++ utf-8-string-literals/Utility.cs
@@ -0,0 +1,9 @@
+﻿namespace utf_8_string_literals;
+
+public class Utility
+{
+    public static void Print(ReadOnlySpan<byte> text)
+    {
+        Console.WriteLine(string.Join(" ", text.ToArray().Select(x => $"0x{x}")));
+    }
+}

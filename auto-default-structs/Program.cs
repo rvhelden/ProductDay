4003b16a1f95d68aa411f5e143bc84a6c6aa75fe
Ronald van Helden <ronald.van.helden@wortell.nl>
2023-01-18 08:34:00 +0100

    Revert "Remove auto"
    This reverts commit e3609bf7ee0f02d159bff48e8d66c84f4a86e805.

--- auto-default-structs/Program.cs
+++ auto-default-structs/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿var value = new IntValue(5);
+
+internal struct IntValue
+{
+    public int Number;
+
+    public IntValue()
+    {
+    }
+
+    public IntValue(int number)
+    {
+        this.Number = number;
+    }
+}

4c830d7bc4cf68dd2b9516398cc748c801986181
Ronald van Helden <ronald.van.helden@wortell.nl>
2023-01-18 15:16:26 +0100

    Revert "Remove raw string literals"
    This reverts commit 20cb687c136c3bcb56551a3d96786785a34067e0.

--- raw-string-literals/Program.cs
+++ raw-string-literals/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// language=json
+var json = """
+{
+    "headers": [
+      { "name": "Authorization", "value": "Bearer xxxxxxxxxxxxxxxxx" }
+    ],
+    "body": ""
+}
+""";
+
+Console.WriteLine(json);
+
+var id = 1;
+// language=xml
+var xml = $$"""
+<Root>
+    <Entities>
+        <Entity>
+            <Field name="id" value="{{id}}" />
+        </Entity>
+    </Entities>
+</Root>
+""";
+
+Console.WriteLine(xml);
